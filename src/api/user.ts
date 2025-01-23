d53851e78b647aadbac05cb0f92b9344f2fa2b76
Anton Pegov <antonpegov@gmail.com>
2025-01-23 21:18:59 +0100

    [8] play with envs

--- src/api/user.ts
+++ src/api/user.ts
@@ -8,6 +8,10 @@ const API = process.env.REACT_APP_API_URL
 
 export const login = async ({ login, password }: LoginCredentials): Promise<string> => {
   console.log('API:', API)
+  console.log('NODE_ENV', process.env.NODE_ENV)
+  console.log('NIXPACKS_BUILD_CMD', process.env.NIXPACKS_BUILD_CMD)
+  console.log('NIXPACKS_BUILD_ENV', process.env.NIXPACKS_BUILD_ENV)
+  console.log('NPM_CONFIG_FUND', process.env.NPM_CONFIG_FUND)
 
   try {
     const response = await axios.post<LoginResponse>(`${API}/auth/organisation`, {
