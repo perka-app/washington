705627ed4eaadc7196b67fbb25e6cee7db32886e
Anton Pegov <antonpegov@gmail.com>
2025-01-23 22:02:38 +0100

    [8] Finally works!

--- src/api/user.ts
+++ src/api/user.ts
@@ -4,14 +4,10 @@ import { LoginCredentials } from 'models/LoginCredentials'
 import { LoginResponse } from 'models/LoginResponse'
 import { User } from 'models/UserModel'
 
-const API = process.env.REACT_APP_API_URL
+const API = `https://${process.env.REACT_APP_API_URL}`
 
 export const login = async ({ login, password }: LoginCredentials): Promise<string> => {
   console.log('API:', API)
-  console.log('NODE_ENV', process.env.NODE_ENV)
-  console.log('NIXPACKS_BUILD_CMD', process.env.NIXPACKS_BUILD_CMD)
-  console.log('NIXPACKS_BUILD_ENV', process.env.NIXPACKS_BUILD_ENV)
-  console.log('NPM_CONFIG_FUND', process.env.NPM_CONFIG_FUND)
 
   try {
     const response = await axios.post<LoginResponse>(`${API}/auth/organisation`, {
