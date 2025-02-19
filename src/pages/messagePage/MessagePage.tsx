5ab82f85621e30b3814cab9f86d5f13d76642d2c
Anton Pegov <antonpegov@gmail.com>
2025-02-19 15:04:50 +0100

    22 | Add Tyni MCE to message page

--- src/pages/messagePage/MessagePage.tsx
+++ src/pages/messagePage/MessagePage.tsx
@@ -1,11 +1,17 @@
+import React, { useRef } from 'react'
 import { useNavigate } from 'react-router'
+import { Editor } from '@tinymce/tinymce-react'
 import { Button } from '@mui/material'
 import { cn } from '@bem-react/classname'
-import React from 'react'
 
 export const MessagePage: React.FC = () => {
-  const bem = cn('User')
-
+  const bem = cn('MesagePage')
+  const editorRef = useRef<any>(null)
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current ? editorRef.current.getContent() : '')
+    }
+  }
   const navigate = useNavigate()
 
   return (
@@ -20,6 +26,46 @@ export const MessagePage: React.FC = () => {
       >
         {'<'} Dashboard
       </Button>
+
+      <Editor
+        apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
+        // eslint-disable-next-line immutable/no-mutation
+        onInit={(_evt, editor) => (editorRef.current = editor)}
+        initialValue="<p>This is the initial content of the editor.</p>"
+        init={{
+          height: 500,
+          menubar: false,
+          plugins: [
+            'advlist',
+            'autolink',
+            'lists',
+            'link',
+            'image',
+            'charmap',
+            'preview',
+            'anchor',
+            'searchreplace',
+            'visualblocks',
+            'code',
+            'fullscreen',
+            'insertdatetime',
+            'media',
+            'table',
+            'code',
+            'help',
+            'wordcount',
+          ],
+          toolbar:
+            'undo redo | blocks | ' +
+            'bold italic forecolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+          content_style:
+            'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+        }}
+      />
+
+      <button onClick={log}>Log editor content</button>
     </div>
   )
 }
