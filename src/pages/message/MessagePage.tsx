ebdbcebc6fad3cfdb1fd0e87845a465eba84d43c
Anton Pegov <antonpegov@gmail.com>
2025-02-22 15:55:17 +0100

    24 | Implement sending test message

--- src/pages/message/MessagePage.tsx
+++ src/pages/message/MessagePage.tsx
@@ -1,8 +1,11 @@
+// #region Imports
+// eslint-disable max-lines
+/* prettier-ignore */ import { Box, Button, IconButton, Input, Modal, Paper, Stack, Switch, Tooltip, Typography } from '@mui/material'
 import { useRef, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Button, Input } from '@mui/material'
 import { useNavigate } from 'react-router'
 import { Editor } from '@tinymce/tinymce-react'
+import { Close } from '@mui/icons-material'
 import { cn } from '@bem-react/classname'
 
 import { DebugInfo } from 'shared/components'
@@ -11,7 +14,8 @@ import { messageActions } from 'state/message/message.slice'
 import { sendingMessageProcessSelector } from 'state/message/message.selectors'
 
 import './MessagePage.scss'
-
+// eslint-enable max-lines
+// #endregion
 export const MessagePage: React.FC = () => {
   const bem = cn('MessagePage')
   const navigate = useNavigate()
@@ -19,27 +23,55 @@ export const MessagePage: React.FC = () => {
   const editorRef = useRef<any>(null)
   const processing = useSelector(sendingMessageProcessSelector)
 
+  const [modalChecked, setModalChecked] = useState(false)
   const [renderCount, setRenderCount] = useState<number>(0)
+  const [modalShown, setModalShown] = useState(false)
   const [message, setMessage] = useState<string | null>(null)
   const [error, setError] = useState<string | null>(null)
   const [title, setTitle] = useState('')
+  const [email, setEmail] = useState('')
 
   // #region Handlers
-  const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+  const handleTitleChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
     setTitle(event.target.value)
   }
 
-  const handleSaveMessage = () => {
-    if (!!message && !!title) {
-      dispatch(messageActions.sendMessage({ title, message }))
-    }
+  const handleShowModal = (): void => {
+    setModalShown(true)
   }
 
-  const handleEditorChange = (content: string) => {
+  const handleHideModal = (): void => {
+    setModalShown(false)
+  }
+
+  const handleModalChecked = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setModalChecked(event.target.checked)
+  }
+
+  const handleSaveMessage = (): void => {
+    if (!message || !title) {
+      setError('Message and title are required')
+      return
+    }
+
+    if (modalChecked) {
+      dispatch(messageActions.sendMessage({ title, message }))
+      return
+    }
+
+    dispatch(messageActions.sendTestMessage({ title, message, email }))
+  }
+
+  const handleEditorChange = (content: string): void => {
     setMessage(content)
+
     if (editorRef.current) {
       // console.log(editorRef.current ? editorRef.current.getContent() : '')
     }
+  }
+
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setEmail(event.target.value)
   }
   // #endregion
   // #region Hooks
@@ -67,7 +99,7 @@ export const MessagePage: React.FC = () => {
         {'<'} Dashboard
       </Button>
 
-      <div className={bem('TitleRow')}>
+      <Stack direction="row" className={bem('TitleRow')}>
         <Input
           className={bem('Title')}
           placeholder="Message title"
@@ -76,16 +108,16 @@ export const MessagePage: React.FC = () => {
         />
 
         <Button
-          className={bem('Submit')}
+          className={bem('NextButton')}
           loading={processing.pending}
           loadingPosition="start"
           variant="contained"
           disabled={!message || !title || processing.pending}
-          onClick={handleSaveMessage}
+          onClick={handleShowModal}
         >
-          Save
+          Next
         </Button>
-      </div>
+      </Stack>
 
       <Editor
         apiKey={process.env.REACT_APP_TINYMCE_API_KEY}
@@ -128,6 +160,52 @@ export const MessagePage: React.FC = () => {
 
       {error && <div className={bem('Error')}>Error: {error}</div>}
       <DebugInfo info={`rendered ${renderCount} times`} />
+
+      <Modal open={modalShown}>
+        <Box className={bem('Modal')}>
+          <IconButton className={bem('ModalClose')} onClick={handleHideModal}>
+            <Close />
+          </IconButton>
+
+          <Paper className={bem('ModalPreview')}>
+            <Typography variant="h6">{title}</Typography>
+            <div dangerouslySetInnerHTML={{ __html: message || '' }} />
+          </Paper>
+
+          <div className={bem('ModalContent')}>
+            <Stack direction="row" className={bem('ModalSwitch')}>
+              <Tooltip title="Send test message to specified email">
+                <Typography>Test</Typography>
+              </Tooltip>
+
+              <Switch
+                checked={modalChecked}
+                onChange={handleModalChecked}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+
+              <Typography>Send to subscribers</Typography>
+            </Stack>
+
+            <Input
+              className={bem('ModalEmailInput')}
+              placeholder="Email for test message"
+              disabled={modalChecked}
+              value={email}
+              onChange={handleEmailChange}
+            />
+
+            <Button
+              className={bem('ModalSubmit')}
+              variant="contained"
+              loading={processing.pending}
+              onClick={handleSaveMessage}
+            >
+              Send
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   )
 }
